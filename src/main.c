f101437e84560acabe1f040f430f2bf8a69e3920
darkerbit <mail.darkerbit@gmail.com>
2022-09-22 17:04:46 +0300

    Change default speed

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
 	// Parse flags
 	int vertical = 0;
-	unsigned int speed = 1000;
+	unsigned int speed = 10000;
 
 	int i;
 	for (i = 1; i < argc && argv[i][0] == '-'; ++i)
